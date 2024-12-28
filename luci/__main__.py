b2ea74a509cdb3bcba7da5cc853269763d1e5e6b
Christian Decker <christian.decker@lookslikematrix.de>
2024-12-28 06:44:36 +0100

    Add `erase` command and `--scale` parameter.

--- luci/__main__.py
+++ luci/__main__.py
@@ -7,42 +7,16 @@ import trimesh
 import miney
 import numpy as np
 
-
-@click.group()
-@click.option(
-    "--loglevel",
-    help="Set loglevel (default: WARNING)"
-)
-def cli(loglevel):
-    """📦️ LuCI - Luanti Commandline Interface 📦️
-
-    Read here for further information:\n
-        https://github.com/lookslikematrix/luci
-
-    If you've any issues report them here:\n
-        https://github.com/lookslikematrix/luci/issues/new
-    """
-    if loglevel:
-        logging.basicConfig(encoding='utf-8', level=loglevel)
-    else:
-        logging.disable(logging.CRITICAL)
-
-@cli.command()
-@click.argument('filename')
-def build(filename: str):
-    """Build a STL file into Luanti.
-
-Example:
-
-    Build a STL file.
-        luci build path_to_stl_file.stl
-    """
+def get_object_matrix(filename: str, scale: float):
     stl_file = Path(filename)
     click.echo("[%s] 📃 Read STL file." % stl_file.absolute())
     stl_mesh = trimesh.load_mesh(stl_file)
 
     click.echo("🎲 Create a matrix from the STL file.")
-    voxel_size = 0.3
+    if scale == 0:
+        raise ValueError("[%s] scale could not be zero." % scale)
+    logging.debug("[%s] scale value." % scale)
+    voxel_size = 1 / scale
     voxelized = stl_mesh.voxelized(voxel_size)
     voxel_matrix = voxelized.matrix
 
@@ -71,39 +45,16 @@ Example:
     # multiply with rotation matrix
     rotated_three_x_n_matrix = np.dot(four_x_n_marix[:3].T, rotation_matrix).T
 
-    # print in luanti
     # separate air and object
     object_values = four_x_n_marix[3, :]
     # Find indices where the value is 1
     object_indices = np.where(object_values == 1)[0]
-    air_indices = np.where(object_values == 0)[0]
 
     object_matrix = rotated_three_x_n_matrix[:3, object_indices]
-    air_matrix = rotated_three_x_n_matrix[:3, air_indices]
     click.echo("[ %s ] object pixels." % len(object_indices))
-    click.echo("[ %s ] air pixels." % len(air_indices))
+    return object_matrix
 
-    def print_voxels(mt, matrix, type):
-        batch_size = 32000
-        batches = list()
-        batch_index = 0
-        max_batches = matrix.T.shape[0] // batch_size
-        for object_pixel in matrix.T:
-            if len(batches) > batch_size:
-                batch_index += 1
-                click.echo("[ %s/%s ] batch index." % (batch_index, max_batches))
-                mt.node.set(batches, name=type)
-                batches = list()
-            batches.append(
-                {
-                    "x": int(object_pixel[0, 0]),
-                    "y": int(object_pixel[0, 1]),
-                    "z": int(object_pixel[0, 2])
-                }
-            )
-        mt.node.set(batches, name=type)
-
-    mt = miney.Minetest()
+def init_luanti_player(mt):
     mt.time_of_day = 0.3
     mt.player["singleplayer"].speed = 50
     mt.player["singleplayer"].fly = True
@@ -113,8 +64,87 @@ Example:
         "z": 0
     }
 
-    print_voxels(mt, object_matrix, "default:goldblock")
-    print_voxels(mt, air_matrix, "air")
+def build_voxels(mt, matrix, type):
+    batch_size = 32000
+    batches = list()
+    batch_index = 0
+    max_batches = matrix.T.shape[0] // batch_size
+    for object_pixel in matrix.T:
+        if len(batches) > batch_size:
+            batch_index += 1
+            click.echo("[ %s/%s ] batch index." % (batch_index, max_batches))
+            mt.node.set(batches, name=type)
+            batches = list()
+        batches.append(
+            {
+                "x": int(object_pixel[0, 0]),
+                "y": int(object_pixel[0, 1]),
+                "z": int(object_pixel[0, 2])
+            }
+        )
+    mt.node.set(batches, name=type)
+
+@click.group()
+@click.option(
+    "--loglevel",
+    help="Set loglevel (default: WARNING)"
+)
+def cli(loglevel):
+    """📦️ LuCI - Luanti Commandline Interface 📦️
+
+    Read here for further information:\n
+        https://github.com/lookslikematrix/luci
+
+    If you've any issues report them here:\n
+        https://github.com/lookslikematrix/luci/issues/new
+    """
+    if loglevel:
+        logging.basicConfig(encoding='utf-8', level=loglevel)
+    else:
+        logging.disable(logging.CRITICAL)
+
+@cli.command()
+@click.argument('filename')
+@click.option(
+    "-s", "--scale",
+    help="Specifies how the object should be scaled.",
+    default=1,
+    show_default=True
+)
+def build(filename: str, scale: float):
+    """Build a STL file into Luanti.
+
+Example:
+
+    Build a STL file.
+        luci build path_to_stl_file.stl
+    """
+    object_matrix = get_object_matrix(filename, scale)
+    mt = miney.Minetest()
+    init_luanti_player(mt)
+    build_voxels(mt, object_matrix, "default:goldblock")
+
+
+@cli.command()
+@click.argument('filename')
+@click.option(
+    "-s", "--scale",
+    help="Specifies how the object should be scaled.",
+    default=1,
+    show_default=True
+)
+def erase(filename: str, scale: float):
+    """Erase a STL file from Luanti.
+
+Example:
+
+    Erase a STL file.
+        luci erase path_to_stl_file.stl
+    """
+    object_matrix = get_object_matrix(filename, scale)
+    mt = miney.Minetest()
+    init_luanti_player(mt)
+    build_voxels(mt, object_matrix, "air")
 
 
 if __name__ == '__main__':
